752ad725c0595d2838b8a28183195bda20ab00b1
Minseok <cms7371@gmail.com>
2021-02-15 17:45:28 +0900

    경로 탐색 알고리즘 복습

---
+++ 5.ShortestPath/0.1.BasicDijkstra.py
@@ -0,0 +1,57 @@
+# Dijkstra Algorism
+INF = int(1e9)
+
+# 노드 개수와 간선 개수 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호 입력 받기
+start = int(input())
+# 각 노드의 연결 정보 리스트 만들기
+graph = [[] for i in range(n + 1)]
+# 방문한 적이 있는지 체크하는 리스트 만들기
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+
+def get_smallest_node():
+    min_value = INF
+    index = 0 # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+        return index
+
+
+def dijkstra(s):
+    # 시작 노드에 대해서 초기화
+    distance[s] = 0
+    visited[s] = True
+    for j in graph[s]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n - 1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now] + j[i]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+
---
+++ 5.ShortestPath/0.3.FloydWarshall.py
@@ -0,0 +1,28 @@
+INF = int(1e9)
+
+# 노드와 간선의 개수를 입력받기
+n = int(input())
+m = int(input())
+# 2차원 리스트(그래프를 나타냄)를 만들고, 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신으로 가는 비용은 0으로
+for i in range(1, n + 1):
+    graph[i][i] = 0
+# 간선 정보를 입력받아 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용은 C라고 설정
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
